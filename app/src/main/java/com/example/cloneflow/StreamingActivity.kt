b01ce2271df131211ace31467a33a2a4db46f42e
SeojinSeojin <1106laura@naver.com>
2021-02-03 23:05:53 +0900

    [Feat] 가사 기능 추가

--- app/src/main/java/com/example/cloneflow/StreamingActivity.kt
+++ app/src/main/java/com/example/cloneflow/StreamingActivity.kt
@@ -1,17 +1,13 @@
 package com.example.cloneflow
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.media.AudioAttributes
-import android.media.AudioManager
 import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.provider.MediaStore
 import android.util.Log
 import android.widget.ImageButton
 import android.widget.SeekBar
@@ -27,7 +23,6 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import kotlin.properties.Delegates
 
 class StreamingActivity : AppCompatActivity() {
 
@@ -37,15 +32,44 @@ class StreamingActivity : AppCompatActivity() {
         var isPlaying : Boolean = false
         var isFirst : Boolean = true
         var pos : Int? = null
+        var lyrics : String? = null
+        var lastLyricPos : Int? = null
+        lateinit var lyricsMap : Map<Int, String>
         lateinit var mediaPlayer : MediaPlayer
         lateinit var seekbar : SeekBar
         lateinit var handleBtn : ImageButton
+        lateinit var currentLyricsTextView : TextView
+        lateinit var nextLyricsTextView: TextView
     }
 
     private class StreamingThread : Thread() {
+        fun milisecToString(miliseconds : Int) : String {
+            val minutes : Int = (miliseconds / 1000 / 60)
+            val seconds : Int = (miliseconds / 1000 % 60)
+            val minutesString = String.format("%02d", minutes)
+            val secondsString = String.format("%02d", seconds)
+            return "$minutesString:$secondsString"
+        }
+        fun searchLyrics(curPos : Int) : Int {
+            val timeStampKeys = lyricsMap.keys.toList()
+            println(timeStampKeys)
+            var curLyricPos : Int = -1
+            for((i, item) in timeStampKeys.withIndex()) { // i는 인덱스, item은 시간
+                if(curPos>=item && timeStampKeys[i+1]>=curPos) {
+                    curLyricPos = i
+                    break
+                }
+            }
+            return curLyricPos
+        }
         override fun run() {
             while(isPlaying) {
                 seekbar.progress = mediaPlayer.currentPosition
+                val curLyrics = searchLyrics(mediaPlayer.currentPosition)
+                if(curLyrics != -1){
+                    currentLyricsTextView.text = lyricsMap[lyricsMap.keys.toList()[curLyrics]]
+                    if(curLyrics != lastLyricPos!!-2) {nextLyricsTextView.text = lyricsMap[lyricsMap.keys.toList()[curLyrics+1]]}
+                }
             }
         }
     }
@@ -81,9 +105,15 @@ class StreamingActivity : AppCompatActivity() {
                                 val result = responseBody.result!!
                                 findViewById<TextView>(R.id.streaming_title).text = result.title.toString()
                                 findViewById<TextView>(R.id.streaming_artist).text = result.artist.toString()
-                                findViewById<TextView>(R.id.streaming_lyric).text = result.lyrics.toString()
+                                //findViewById<TextView>(R.id.streaming_lyric).text = result.lyrics.toString()
                                 Glide.with(this@StreamingActivity).load(result.cover.toString())
                                     .into(findViewById(R.id.streaming_albumcover))
+                                lyrics = result.lyrics.toString()
+                                lyricsMap = parseByRN(lyrics!!)
+                                Log.d("로그", "lyricsmap $lyricsMap")
+                                lastLyricPos = lyricsMap.size
+                                currentLyricsTextView = findViewById(R.id.lyrics_now)
+                                nextLyricsTextView = findViewById(R.id.lyrics_next)
                                 mediaPlayer = MediaPlayer()
                                 seekbar = findViewById(R.id.streaming_seekbar)
                                 handleBtn = findViewById(R.id.streaming_handle_btn)
@@ -114,7 +144,7 @@ class StreamingActivity : AppCompatActivity() {
                                         pauseStreaming()
                                     } else { // start를 누른 경우
                                         if(isFirst) { // 처음 트는 경우
-                                            startStreaming()
+                                            startStreaming(result.src!!.toString())
                                         } else { // 멈췄다가 다시 트는 경우
                                             restartStreaming()
                                         }
@@ -143,6 +173,10 @@ class StreamingActivity : AppCompatActivity() {
         if(mediaPlayer!=null){
             mediaPlayer.release()
         }
+        // companion object는 초기화 해줘야 함
+        isPlaying = false
+        isFirst = true
+        pos = null
     }
 
     private fun getToken(): String? {
@@ -151,9 +185,10 @@ class StreamingActivity : AppCompatActivity() {
     }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    private fun startStreaming(){
+    private fun startStreaming(src : String){
         Log.d("로그", "StreamingActivity - startStreaming() called")
-        val URI = Uri.parse("https://r2---sn-3u-bh2e.googlevideo.com/videoplayback?expire=1612366063&ei=j2waYP75JbiFlQTrlJW4Bg&ip=203.229.147.89&id=o-AKdRJUF0WqnRXi1k08yTblD0lUH5CZZ6wL84eSZ6NBwE&itag=140&source=youtube&requiressl=yes&hcs=yes&mh=v-&mm=31%2C29&mn=sn-3u-bh2e%2Csn-3u-bh2e&ms=au%2Crdu&mv=m&mvi=2&pcm2cms=yes&pl=19&rmhost=r7---sn-3u-bh2e.googlevideo.com&shardbypass=yes&smhost=r7---sn-3u-bh2ll.googlevideo.com&initcwndbps=863750&vprv=1&mime=audio%2Fmp4&ns=2daCWx3AD1bJFYMdarg0byAF&gir=yes&clen=3207289&dur=198.135&lmt=1611952280808383&mt=1612344286&fvip=7&keepalive=yes&c=WEB&txp=5531432&n=Jy6Iv7w9mas2ElTwL&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cns%2Cgir%2Cclen%2Cdur%2Clmt&lsparams=hcs%2Cmh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpcm2cms%2Cpl%2Crmhost%2Cshardbypass%2Csmhost%2Cinitcwndbps&lsig=AG3C_xAwRQIgVXm-hHv4EW6C7aNfW8ueNmx83X4PmTf7U5GSEmZRNTgCIQCD_asWXqvkcW6QrC3qBg1mr_m_loGthjkcT3Gn9yB-kQ%3D%3D&sig=AOq0QJ8wRQIgJmW3r4O9WfwiZZEfsbfvD2gYbl6icLoUUn4vTL6K9AMCIQDWIPI7PKIsieT7DRPbqDJ2DJ82QunUuz369y5g7Qe5Gw==&ratebypass=yes")
+        //val URI = Uri.parse("https://r2---sn-3u-bh2e.googlevideo.com/videoplayback?expire=1612366063&ei=j2waYP75JbiFlQTrlJW4Bg&ip=203.229.147.89&id=o-AKdRJUF0WqnRXi1k08yTblD0lUH5CZZ6wL84eSZ6NBwE&itag=140&source=youtube&requiressl=yes&hcs=yes&mh=v-&mm=31%2C29&mn=sn-3u-bh2e%2Csn-3u-bh2e&ms=au%2Crdu&mv=m&mvi=2&pcm2cms=yes&pl=19&rmhost=r7---sn-3u-bh2e.googlevideo.com&shardbypass=yes&smhost=r7---sn-3u-bh2ll.googlevideo.com&initcwndbps=863750&vprv=1&mime=audio%2Fmp4&ns=2daCWx3AD1bJFYMdarg0byAF&gir=yes&clen=3207289&dur=198.135&lmt=1611952280808383&mt=1612344286&fvip=7&keepalive=yes&c=WEB&txp=5531432&n=Jy6Iv7w9mas2ElTwL&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cns%2Cgir%2Cclen%2Cdur%2Clmt&lsparams=hcs%2Cmh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpcm2cms%2Cpl%2Crmhost%2Cshardbypass%2Csmhost%2Cinitcwndbps&lsig=AG3C_xAwRQIgVXm-hHv4EW6C7aNfW8ueNmx83X4PmTf7U5GSEmZRNTgCIQCD_asWXqvkcW6QrC3qBg1mr_m_loGthjkcT3Gn9yB-kQ%3D%3D&sig=AOq0QJ8wRQIgJmW3r4O9WfwiZZEfsbfvD2gYbl6icLoUUn4vTL6K9AMCIQDWIPI7PKIsieT7DRPbqDJ2DJ82QunUuz369y5g7Qe5Gw==&ratebypass=yes")
+        val URI = Uri.parse(src)
         val audioAttributes = AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_MOVIE).build()
         mediaPlayer.setAudioAttributes(audioAttributes)
         mediaPlayer.reset()
@@ -192,4 +227,19 @@ class StreamingActivity : AppCompatActivity() {
         handleBtn.setImageDrawable(ContextCompat.getDrawable(this@StreamingActivity, R.drawable.ic_play_button_big))
     }
 
+    private fun stringToMilisec(miliStr : String) : Int {
+        val minutes : Int = miliStr.slice(IntRange(0,1)).toInt()
+        val seconds : Int = miliStr.slice(IntRange(3,4)).toInt()
+        return (minutes * 60 + seconds) * 1000
+    }
+    private fun parseByRN(responseLyrics : String) : Map<Int, String> {
+        val parsedList = responseLyrics.split("\r\n")
+        val lyricsMap = mutableMapOf<Int, String>()
+        for((i, _) in parsedList.withIndex()){
+            if(i%2==0){
+                lyricsMap[stringToMilisec(parsedList[i])] = parsedList[i+1]
+            }
+        }
+        return lyricsMap
+    }
 }
